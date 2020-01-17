1b0486cd992b352dc69459272419bb6d0ea8d26c
Youyang Feng <fyy623@uw.edu>
2020-01-17 12:47:38 -0800

    1st commit

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -5,14 +5,12 @@
 add_three <- function(num) {
   return (num + 3)
 } 
-print(add_three(1))
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
 ten <- function(num) {
   return (add_three(num))
 }
-print(ten(7))
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
@@ -20,8 +18,7 @@ print(ten(7))
 imperial_to_metric <- function(feet, inch) {
   return (0.3048 * feet + 0.0254 * inch)
 }
-print(imperial_to_metric(1, 1))
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
-
+height_in_meters <- imperial_to_metric(5, 7)
